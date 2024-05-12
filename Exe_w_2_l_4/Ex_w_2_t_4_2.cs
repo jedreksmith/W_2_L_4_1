1ca15f774c4013e0a6ecdb53d9cb969555278396
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 21:38:21 +0200

    Add project files.

---
+++ Exe_w_2_l_4/Ex_w_2_t_4_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_w_2_l_4
+{
+    internal class Ex_w_2_t_4_2
+    {
+    }
+}
---
+++ Exe_w_2_l_4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exe_w_2_l_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string SecondName;
+
+            string FirstName;
+
+            int Age;
+
+            char Sex;
+
+            string PESEL;
+
+            int NumberOfWokers;
+
+            Console.WriteLine("Good or bed");
+        }
+    }
+}
